57ae6819e4d872e6653ff53d0fa5831e7a2911b7
Astuto0519 <atsuto.fps@gmail.com>
2017-06-15 17:18:53 +0900

    Change files

--- Fourier_transform2.py
+++ Fourier_transform2.py
@@ -5,7 +5,7 @@ import math
 import matplotlib.pyplot as plt
 
 # フーリエ変換の関数
-def fourier_func(x, t, l) :
+def fourier_func(x, t, T) :
     res = []
     c_0 = 0
     
@@ -15,17 +15,17 @@ def fourier_func(x, t, l) :
         N = np.array(range(K))+1
         for n in N :
             a_n = 0
-            b_n = (-2*((-1)**n))/(n*math.pi)
-            res[j] += b_n*np.sin((n*math.pi*x_i)/l)
+            b_n = (2*(-1)**(n+1))/(n*math.pi)
+            res[j] += a_n*np.cos(n*(x_i+t)*math.pi*2 / T) + b_n*np.sin(n*(x_i+t)*math.pi*2 / T)
         j += 1
     return res
 
 # 級数を用意
 x = np.arange(0, 10, 0.1)
-K = 50
+K = 10
 t = 0
 T = 4
 
 # プロット
-plt.plot(x, fourier_func(x, t, T/2))
+plt.plot(x, fourier_func(x, t, T))
 plt.show()
